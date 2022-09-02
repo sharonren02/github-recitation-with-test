c1576eb2c8bdcdeaae241a0b7df15e854c18d8e5
Sharon Ren <sharonren06@gmail.com>
2022-09-02 14:11:48 -0400

    fixed negative fibonacci

--- fib.py
+++ fib.py
@@ -8,6 +8,8 @@ Negative numbers should return None
 def fibonacci(position):
   if(position == 1 or position == 2):
     return 1
+  elif(position < 0): 
+    return None; 
   return fibonacci(position - 1) + fibonacci(position - 2)
 
 
